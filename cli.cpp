232a6767e4cc6f8036a72f07487cf13144547ef5
Nikita Sychev <root@nsychev.ru>
2018-06-17 11:46:42 +0300

    fix CLI

--- cli.cpp
+++ cli.cpp
@@ -10,38 +10,12 @@ enum huffman_mode_t {
 
 void print_usage() {
     std::cerr << "\033[1;32mhuffman\033[0m - something about data compression\n\n"
-              << "Usage: huffman [mode] [<input file> [<output file>]]\n\n"
+              << "Usage: huffman [mode <input file> [<output file>]]\n\n"
               << "Available modes: \n"
               << "  -c, --compress\n"
               << "  -d, --decompress\n"
               << "  -h, --help (default)\n\n"
-              << "If only output file is not specified, output is written to <input file>.out.\n"
-              << "If both files are not specified, input is STDIN and output is STDOUT." << std::endl;
-}
-
-int process(huffman_mode_t mode, std::istream& in, std::ostream& out) {
-    try {
-        clock_t begin = clock();
-
-        if (mode == COMPRESS) {
-            std::cerr << "Starting compressing.." << std::endl;
-            huffman::compress(in, out);
-        } else {
-            std::cerr << "Starting decompressing.." << std::endl;
-            huffman::decompress(in, out);
-        }
-
-        clock_t end = clock();
-
-        std::cerr << "\n\033[1;32mFinished\033[0m in "
-                  << std::fixed << std::setprecision(3) << double(end - begin) / CLOCKS_PER_SEC
-                  << " s" << std::endl;
-
-        return 0;
-    } catch (std::exception &e) {
-        std::cerr << "\033[1;31m[ERROR]\033[0m " << e.what() << std::endl;
-        return 2;
-    }
+              << "If only output file is not specified, output is written to <input file>.out." << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -66,35 +40,52 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc == 2) {
-        return process(mode, std::cin, std::cout);
-    } else {
-        std::string input_path = argv[2];
-        std::string output_path;
+        std::cerr << "Input file is not specified" << std::endl;
+        return 1;
+    }
 
-        if (argc == 4) {
-            output_path = argv[3];
+    std::string input_path = argv[2];
+    std::string output_path;
 
-            if (input_path == output_path) {
-                std::cerr << "Please specify differerent input and output files" << std::endl;
-                return 2;
-            }
-        } else {
-            output_path = input_path + ".out";
-        }
+    if (argc == 4) {
+        output_path = argv[3];
 
-        try {
-            std::ifstream input_file(input_path, std::ios::in | std::ios::binary);
-            if (input_file.fail())
-                throw std::runtime_error("cannot open " + input_path);
-
-            std::ofstream output_file(output_path, std::ios::out | std::ios::binary);
-            if (output_file.fail())
-                throw std::runtime_error("cannot open " + output_path);
-
-            return process(mode, input_file, output_file);
-        } catch (std::exception &e) {
-            std::cerr << "\033[1;31m[ERROR]\033[0m " << e.what() << std::endl;
+        if (input_path == output_path) {
+            std::cerr << "Please specify differerent input and output files" << std::endl;
             return 2;
         }
+    } else {
+        output_path = input_path + ".out";
+    }
+
+    try {
+        std::ifstream input_file(input_path, std::ios::in | std::ios::binary);
+        if (input_file.fail())
+            throw std::runtime_error("cannot open " + input_path);
+
+        std::ofstream output_file(output_path, std::ios::out | std::ios::binary);
+        if (output_file.fail())
+            throw std::runtime_error("cannot open " + output_path);
+
+        clock_t begin = clock();
+
+        if (mode == COMPRESS) {
+            std::cerr << "Starting compressing.." << std::endl;
+            huffman::compress(in, out);
+        } else {
+            std::cerr << "Starting decompressing.." << std::endl;
+            huffman::decompress(in, out);
+        }
+
+        clock_t end = clock();
+
+        std::cerr << "\n\033[1;32mFinished\033[0m in "
+                  << std::fixed << std::setprecision(3) << double(end - begin) / CLOCKS_PER_SEC
+                  << " s" << std::endl;
+
+        return 0;
+    } catch (std::exception &e) {
+        std::cerr << "\033[1;31m[ERROR]\033[0m " << e.what() << std::endl;
+        return 2;
     }
 }
